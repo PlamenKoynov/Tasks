e6d8db4bb70aa6c853670532b6e79a42ac895d6a
PlamenKoynov <plamen_bul_dg@abv.bg>
2014-07-18 10:53:36 +0300

    Strawberries

---
+++ strawberries.pl
@@ -0,0 +1,126 @@
+#!usr/bin/perl -w
+use strict;
+
+my @strawberries = ();
+my $rows;
+my $cols;
+my $days;
+my $i = 0;
+my $j = 0;
+my $d = 0;
+my $counter = 2;
+my $x;
+my $y;
+my $countStrawberries;
+
+do
+{
+	print "Size x: ";
+	$x = <STDIN>;
+	chomp($x);
+}
+while($x <= 0 || $x > 1000);
+
+do
+{
+	print "Size y: ";
+	$y = <STDIN>;
+	chomp($y);
+}
+while($y <= 0 || $y > 1000);
+do
+{
+	print "Days: ";
+	$days = <STDIN>;
+	chomp($days);
+}
+while($days < 0 || $days > 100);
+do
+{
+	print "Bad strawberries: ";
+	$countStrawberries = <STDIN>;
+	chomp($countStrawberries);
+}
+while($countStrawberries > $x * $y || $countStrawberries < 0);
+
+for($i = 1; $i <= $x; $i++)
+{
+	for($j = 1; $j <= $y; $j++)
+	{
+		$strawberries[$i][$j] = 0;
+	}
+}
+
+
+
+for($i = 0; $i <= $countStrawberries - 1; $i++)
+{
+	do
+	{
+		print "Row: ";
+		$rows = <STDIN>;
+		chomp($rows);
+	}
+	while ($rows < 0 || $rows > $x); 
+	do
+	{
+		print "Column: ";
+		$cols = <STDIN>;
+		chomp($cols);
+	}
+	while ($cols < 0 || $cols > $y);
+	$strawberries[$rows][$cols] = 1;
+}
+
+for($d = 1; $d <= $days; $d++)
+{
+	for($i = 1; $i <= $x; $i++)
+	{
+		for($j = 1; $j <= $y; $j++)
+	  	{
+	  		if($strawberries[$i][$j] == $d)
+	  		{
+	  			if($i + 1 <= $x && $strawberries[$i + 1][$j] == 0)
+	  			{
+	  				$strawberries[$i + 1][$j] = $counter;
+	  			}
+	  			if($i - 1 >= 0 && $strawberries[$i - 1][$j] == 0)
+	  			{
+	  				$strawberries[$i - 1][$j] = $counter;
+	  			}
+	  			if($j + 1 <= $y && $strawberries[$i][$j + 1] == 0)
+	  			{
+	  				$strawberries[$i][$j + 1] = $counter;
+	  			}
+	  			if($j - 1 >= 0 && $strawberries[$i][$j - 1] == 0)
+	  			{
+	  				$strawberries[$i][$j - 1] = $counter;
+	  			}
+	  		}
+	  	}
+	}
+	$counter++;
+}
+
+$counter = 0;
+for($i = 1; $i <= $x; $i++)
+{
+	for($j = 1; $j <= $y; $j++)
+	{
+		if($strawberries[$i][$j] == 0)
+	  	{
+	  		$counter++;
+	  	}
+	}
+}
+
+print "Good strawberries: $counter!\n";
+
+
+
+
+
+
+
+
+
